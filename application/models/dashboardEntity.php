f334a01fdeaa5ba753ef60222387238cddb75211
Syahril Hermana <syahril@jakartawebs.com>
2016-09-20 12:30:34 +0700

    init dashboard

---
+++ application/models/dashboardEntity.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by syaharil.hermana@gmail.com
+ */
+
+use Eloquent\Model as Model;
+
+class DashboardEntity extends Model {
+    protected $table = "mst_akses";
+    protected $primary = "mst_akses_id";
+    var $CI = NULL;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->CI =& get_instance();
+    }
+
+    public function user(){
+        return $this->belongsTo('userEntity', 'mst_akses_id', 'mst_akses_id');
+    }
+
+    public function get_capaian(){
+        return null;
+    }
+}
